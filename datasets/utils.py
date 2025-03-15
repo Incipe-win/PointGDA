07e63906873b300d19668a8103b8b82413ad648a
hchao <huachaowu@qq.com>
2025-03-15 16:41:43 +0800

    :card_file_box: add some data augments

--- datasets/utils.py
+++ datasets/utils.py
@@ -10,7 +10,7 @@ import torch
 from torch.utils.data import Dataset as TorchDataset
 import torchvision.transforms as T
 from PIL import Image
-import multiprocessing
+import numpy as np
 
 
 def read_json(fpath):
@@ -279,19 +279,6 @@ class DatasetBase:
         return output
 
 
-# class DatasetWrapper(TorchDataset):
-#     def __init__(self, data_source, input_size, transform=None, is_train=False, return_img0=False, k_tfm=1):
-#         self.data_source = data_source
-
-#     def __len__(self):
-#         return len(self.data_source)
-
-#     def __getitem__(self, idx):
-#         item = self.data_source[idx]
-#         output = {"label": item.label, "domain": item.domain, "impath": item.impath}
-#         return output["impath"], output["label"]
-
-
 class DatasetWrapper(TorchDataset):
     def __init__(self, data_source, input_size, transform=None, is_train=False, return_img0=False, k_tfm=1):
         self.data_source = data_source
@@ -321,6 +308,96 @@ class DatasetWrapper(TorchDataset):
 
         output = {"label": item.label, "domain": item.domain, "impath": item.impath, "rgb": item.rgb}
         return output["impath"], output["label"], output["rgb"]
+
+    def rotate_point_cloud(batch_data):
+        """Randomly rotate the point clouds to augument the dataset
+        rotation is per shape based along up direction
+        Input:
+        BxNx3 array, original batch of point clouds
+        Return:
+        BxNx3 array, rotated batch of point clouds
+        """
+        rotated_data = np.zeros(batch_data.shape, dtype=np.float32)
+        for k in range(batch_data.shape[0]):
+            rotation_angle = np.random.uniform() * 2 * np.pi
+            cosval = np.cos(rotation_angle)
+            sinval = np.sin(rotation_angle)
+            rotation_matrix = np.array([[cosval, 0, sinval], [0, 1, 0], [-sinval, 0, cosval]])
+            shape_pc = batch_data[k, ...]
+            rotated_data[k, ...] = np.dot(shape_pc.reshape((-1, 3)), rotation_matrix)
+        return rotated_data
+
+    def random_point_dropout(batch_pc, max_dropout_ratio=0.875):
+        """batch_pc: BxNx3"""
+        for b in range(batch_pc.shape[0]):
+            dropout_ratio = np.random.random() * max_dropout_ratio  # 0~0.875
+            drop_idx = np.where(np.random.random((batch_pc.shape[1])) <= dropout_ratio)[0]
+            if len(drop_idx) > 0:
+                batch_pc[b, drop_idx, :] = batch_pc[b, 0, :]  # set to the first point
+        return batch_pc
+
+    def random_scale_point_cloud(batch_data, scale_low=0.8, scale_high=1.25):
+        """Randomly scale the point cloud. Scale is per point cloud.
+        Input:
+            BxNx3 array, original batch of point clouds
+        Return:
+            BxNx3 array, scaled batch of point clouds
+        """
+        B, N, C = batch_data.shape
+        scales = np.random.uniform(scale_low, scale_high, B)
+        for batch_index in range(B):
+            batch_data[batch_index, :, :] *= scales[batch_index]
+        return batch_data
+
+    def shift_point_cloud(batch_data, shift_range=0.1):
+        """Randomly shift point cloud. Shift is per point cloud.
+        Input:
+        BxNx3 array, original batch of point clouds
+        Return:
+        BxNx3 array, shifted batch of point clouds
+        """
+        B, N, C = batch_data.shape
+        shifts = np.random.uniform(-shift_range, shift_range, (B, 3))
+        for batch_index in range(B):
+            batch_data[batch_index, :, :] += shifts[batch_index, :]
+        return batch_data
+
+    def jitter_point_cloud(batch_data, sigma=0.01, clip=0.05):
+        """Randomly jitter points. jittering is per point.
+        Input:
+        BxNx3 array, original batch of point clouds
+        Return:
+        BxNx3 array, jittered batch of point clouds
+        """
+        B, N, C = batch_data.shape
+        assert clip > 0
+        jittered_data = np.clip(sigma * np.random.randn(B, N, C), -1 * clip, clip)
+        jittered_data += batch_data
+        return jittered_data
+
+    def rotate_perturbation_point_cloud(batch_data, angle_sigma=0.06, angle_clip=0.18):
+        """Randomly perturb the point clouds by small rotations
+        Input:
+        BxNx3 array, original batch of point clouds
+        Return:
+        BxNx3 array, rotated batch of point clouds
+        """
+        rotated_data = np.zeros(batch_data.shape, dtype=np.float32)
+        for k in range(batch_data.shape[0]):
+            angles = np.clip(angle_sigma * np.random.randn(3), -angle_clip, angle_clip)
+            Rx = np.array(
+                [[1, 0, 0], [0, np.cos(angles[0]), -np.sin(angles[0])], [0, np.sin(angles[0]), np.cos(angles[0])]]
+            )
+            Ry = np.array(
+                [[np.cos(angles[1]), 0, np.sin(angles[1])], [0, 1, 0], [-np.sin(angles[1]), 0, np.cos(angles[1])]]
+            )
+            Rz = np.array(
+                [[np.cos(angles[2]), -np.sin(angles[2]), 0], [np.sin(angles[2]), np.cos(angles[2]), 0], [0, 0, 1]]
+            )
+            R = np.dot(Rz, np.dot(Ry, Rx))
+            shape_pc = batch_data[k, ...]
+            rotated_data[k, ...] = np.dot(shape_pc.reshape((-1, 3)), R)
+        return rotated_data
 
 
 def build_data_loader(
